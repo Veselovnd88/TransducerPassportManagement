d82f315b1af351a7a0baaf14cc74f1be9c9c9af7
Veselov Nikolay <veselovnd@gmail.com>
2023-08-08 21:52:23 +0300

    1- pdfService test

--- passport-processing/src/test/java/ru/veselov/passportprocessing/service/impl/PdfServiceImplTest.java
+++ passport-processing/src/test/java/ru/veselov/passportprocessing/service/impl/PdfServiceImplTest.java
@@ -1,5 +1,6 @@
 package ru.veselov.passportprocessing.service.impl;
 
+import lombok.SneakyThrows;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,12 +10,17 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.io.buffer.DataBuffer;
+import ru.veselov.passportprocessing.exception.PdfProcessingException;
 import ru.veselov.passportprocessing.service.PdfHttpClient;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @ExtendWith(MockitoExtension.class)
 class PdfServiceImplTest {
+
+    public final static byte[] SOURCE = new byte[]{1, 2, 3, 4};
 
     @Mock
     PdfHttpClient pdfHttpClient;
@@ -28,9 +34,33 @@ class PdfServiceImplTest {
         Mockito.when(mockDataBuffer.asInputStream()).thenReturn(new ByteArrayInputStream(new byte[]{1, 2, 3, 4}));
         Mockito.when(pdfHttpClient.sendRequestForConvertingDocxToPdf(ArgumentMatchers.any()))
                 .thenReturn(mockDataBuffer);
-        byte[] pdfBytes = pdfService.createPdf(new byte[]{1, 2});
+        byte[] pdfBytes = pdfService.createPdf(SOURCE);
 
         Assertions.assertThat(pdfBytes).isNotNull();
+    }
+
+    @Test
+    void shouldThrowExceptionIfDataBufferIsNull() {
+        Mockito.when(pdfHttpClient.sendRequestForConvertingDocxToPdf(ArgumentMatchers.any()))
+                .thenReturn(null);
+
+        Assertions.assertThatThrownBy(() -> pdfService.createPdf(SOURCE))
+                .isInstanceOf(PdfProcessingException.class);
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldThrowExceptionIfCantConvertToPdfBytes() {
+        DataBuffer mockDataBuffer = Mockito.mock(DataBuffer.class);
+        InputStream inputStream = Mockito.mock(InputStream.class);
+        Mockito.when(inputStream.transferTo(ArgumentMatchers.any())).thenThrow(IOException.class);
+        Mockito.when(mockDataBuffer.asInputStream()).thenReturn(inputStream);
+        Mockito.when(pdfHttpClient.sendRequestForConvertingDocxToPdf(ArgumentMatchers.any()))
+                .thenReturn(mockDataBuffer);
+
+        Assertions.assertThatThrownBy(() -> pdfService.createPdf(SOURCE))
+                .isInstanceOf(PdfProcessingException.class);
+        inputStream.close();
     }
 
 }
