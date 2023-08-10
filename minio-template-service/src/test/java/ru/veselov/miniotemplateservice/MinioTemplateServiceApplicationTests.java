21a735c6939f151f163b253b1d7896792f64650c
Veselov Nikolay <veselovnd@gmail.com>
2023-08-10 16:58:19 +0300

    2-minio-service new service, dependencies

---
+++ minio-template-service/src/test/java/ru/veselov/miniotemplateservice/MinioTemplateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.veselov.miniotemplateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinioTemplateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
