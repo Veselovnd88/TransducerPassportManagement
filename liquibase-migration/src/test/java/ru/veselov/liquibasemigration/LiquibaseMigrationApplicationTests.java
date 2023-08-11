ea60166d9461922046043e7196f1eeef62317c99
Veselov Nikolay <veselovnd@gmail.com>
2023-08-11 12:27:52 +0300

    2-1 liquibase in separate module

---
+++ liquibase-migration/src/test/java/ru/veselov/liquibasemigration/LiquibaseMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.veselov.liquibasemigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
