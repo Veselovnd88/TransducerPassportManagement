97f343c931a94bd3551ec5eb78ff0d5dc2ccc7d9
Veselov Nikolay <veselovnd@gmail.com>
2023-08-15 09:00:25 +0300

    feat(2-minio): testing controller-upload

--- minio-template-service/src/test/java/ru/veselov/miniotemplateservice/controller/TemplateControllerTest.java
+++ minio-template-service/src/test/java/ru/veselov/miniotemplateservice/controller/TemplateControllerTest.java
@@ -1,16 +1,24 @@
 package ru.veselov.miniotemplateservice.controller;
 
+import lombok.SneakyThrows;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.BodyInserters;
+import ru.veselov.miniotemplateservice.dto.TemplateDto;
 import ru.veselov.miniotemplateservice.service.PassportTemplateService;
 
 import java.util.UUID;
@@ -20,7 +28,7 @@ class TemplateControllerTest {
 
     private final static String URL = "/api/v1/template";
 
-    public static final byte[] BYTES_OUT = new byte[]{1, 2, 4};
+    public static final byte[] BYTES = new byte[]{1, 2, 4};
 
     WebTestClient webTestClient;
 
@@ -30,6 +38,12 @@ class TemplateControllerTest {
     @InjectMocks
     TemplateController templateController;
 
+    @Captor
+    ArgumentCaptor<MultipartFile> multipartFileArgumentCaptor;
+
+    @Captor
+    ArgumentCaptor<TemplateDto> templateDtoArgumentCaptor;
+
     @BeforeEach
     void init() {
         webTestClient = MockMvcWebTestClient.bindToController(templateController).build();
@@ -38,7 +52,7 @@ class TemplateControllerTest {
     @Test
     void shouldCallPassportTemplateServiceToGetTemplateSource() {
         String templateId = UUID.randomUUID().toString();
-        Mockito.when(passportTemplateService.getTemplate(templateId)).thenReturn(new ByteArrayResource(BYTES_OUT));
+        Mockito.when(passportTemplateService.getTemplate(templateId)).thenReturn(new ByteArrayResource(BYTES));
         webTestClient.get().uri(uriBuilder -> uriBuilder.path(URL).path("/source").path("/" + templateId).build())
                 .exchange().expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_OCTET_STREAM)
@@ -46,8 +60,24 @@ class TemplateControllerTest {
     }
 
     @Test
+    @SneakyThrows
     void shouldCallPassportTemplateServiceToSaveTemplate() {
-        //TODO
+        TemplateDto templateDto = new TemplateDto("name", "801877", "templates");
+        MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
+        multipartBodyBuilder.part("file", BYTES).filename("filename.docx");
+        multipartBodyBuilder.part("template-info", templateDto);
+
+        webTestClient.post().uri(uriBuilder -> uriBuilder.path(URL).path("/upload").build())
+                .body(BodyInserters.fromMultipartData(multipartBodyBuilder.build()))
+                .exchange().expectStatus().isAccepted();
+
+        Mockito.verify(passportTemplateService, Mockito.times(1))
+                .saveTemplate(multipartFileArgumentCaptor.capture(), templateDtoArgumentCaptor.capture());
+        MultipartFile capturedMultipart = multipartFileArgumentCaptor.getValue();
+        TemplateDto capturedTemplateDto = templateDtoArgumentCaptor.getValue();
+        Assertions.assertThat(capturedMultipart.getBytes()).isEqualTo(BYTES);
+        Assertions.assertThat(capturedMultipart.getOriginalFilename()).isEqualTo("filename.docx");
+        Assertions.assertThat(capturedTemplateDto).isEqualTo(templateDto);
     }
 
 }
