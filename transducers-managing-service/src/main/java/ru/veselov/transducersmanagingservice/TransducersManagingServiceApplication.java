620ab8b90620e2138d1f0be32e89deb4b752df70
Veselov Nikolay <veselovnd@gmail.com>
2023-09-12 16:51:53 +0300

    feat(8) config server with file storage

--- transducers-managing-service/src/main/java/ru/veselov/transducersmanagingservice/TransducersManagingServiceApplication.java
+++ transducers-managing-service/src/main/java/ru/veselov/transducersmanagingservice/TransducersManagingServiceApplication.java
@@ -2,8 +2,10 @@ package ru.veselov.transducersmanagingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class TransducersManagingServiceApplication {
 
 	public static void main(String[] args) {
