db22f58cf86f54e21a4eed8ec3dcdddfa462f25c
Veselov Nikolay <veselovnd@gmail.com>
2023-09-12 18:13:40 +0300

    feat(8) config-server

--- passport-processing/src/main/java/ru/veselov/passportprocessing/PassportProcessingApplication.java
+++ passport-processing/src/main/java/ru/veselov/passportprocessing/PassportProcessingApplication.java
@@ -2,8 +2,10 @@ package ru.veselov.passportprocessing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class PassportProcessingApplication {
 
     public static void main(String[] args) {
