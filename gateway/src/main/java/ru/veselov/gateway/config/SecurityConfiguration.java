3340044a14c7597fdcc2017d1d914f42c20994d5
Veselov Nikolay <veselovnd@gmail.com>
2023-09-22 15:29:17 +0300

    feat(17) keycloak and apigateway configuration

--- gateway/src/main/java/ru/veselov/gateway/config/SecurityConfiguration.java
+++ gateway/src/main/java/ru/veselov/gateway/config/SecurityConfiguration.java
@@ -1,11 +1,14 @@
 package ru.veselov.gateway.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -13,13 +16,19 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
-    private final ReactiveClientRegistrationRepository clientRegistrationRepository;
 
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
+    @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
-                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
-                .oauth2Login(Customizer.withDefaults())
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .build();
     }
+
+
 }
