585ac01929afe18c0e6129c7e1d17b3a6f058f71
Veselov Nikolay <veselovnd@gmail.com>
2023-09-27 14:00:02 +0300

    feat(18) granted authorities extractor

--- gateway/src/main/java/ru/veselov/gateway/security/GrantedAuthoritiesExtractor.java
+++ gateway/src/main/java/ru/veselov/gateway/security/GrantedAuthoritiesExtractor.java
@@ -2,25 +2,31 @@ package ru.veselov.gateway.security;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 import java.util.Collection;
-import java.util.Collections;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Slf4j
 public class GrantedAuthoritiesExtractor implements Converter<Jwt, Collection<GrantedAuthority>> {
 
     @Override
-    public Collection<GrantedAuthority> convert(Jwt jwt) {
-        log.debug("Extracting authorities from Jwt");
-        Collection<?> authorities = (Collection<?>)
-                jwt.getClaims().getOrDefault("authorities", Collections.emptyList());
-        return authorities.stream()
-                .map(Object::toString)
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
+    public Collection<GrantedAuthority> convert(@NonNull Jwt jwt) {
+        log.trace("Extracting authorities from Jwt in custom AuthoritiesExtractor");
+        JwtGrantedAuthoritiesConverter basicJwtAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        Collection<GrantedAuthority> convertedScopes = basicJwtAuthoritiesConverter.convert(jwt);
+        List<String> springRoles = jwt.getClaimAsStringList("spring_roles");
+        List<GrantedAuthority> actualRoles = Stream.concat(convertedScopes.stream(),
+                springRoles.stream().filter(role -> role.startsWith("ROLE_"))
+                        .map(SimpleGrantedAuthority::new)
+                        .map(GrantedAuthority.class::cast)).toList();
+        log.trace("Roles adn scopes: {}", actualRoles);
+        return actualRoles;
     }
+
 }
