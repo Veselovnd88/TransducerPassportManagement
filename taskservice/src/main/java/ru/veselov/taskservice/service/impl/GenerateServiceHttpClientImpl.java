ae67ec2c53410c944806f7b592c686523567ea7a
Veselov Nikolay <veselovnd@gmail.com>
2023-11-13 12:07:42 +0300

    feat(25) rest template-1

--- taskservice/src/main/java/ru/veselov/taskservice/service/impl/GenerateServiceHttpClientImpl.java
+++ taskservice/src/main/java/ru/veselov/taskservice/service/impl/GenerateServiceHttpClientImpl.java
@@ -1,16 +1,28 @@
 package ru.veselov.taskservice.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import ru.veselov.taskservice.dto.GeneratePassportsDto;
 import ru.veselov.taskservice.model.Task;
 import ru.veselov.taskservice.service.GenerateServiceHttpClient;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class GenerateServiceHttpClientImpl implements GenerateServiceHttpClient {
+
+    @Value("${generate-service.url}")
+    private String generateServiceUrl;
+
+    private final RestTemplate restTemplate;
+
     @Override
     public boolean sendTaskToPerform(GeneratePassportsDto generatePassportsDto, Task task) {
+       // restTemplate.exchange(generateServiceUrl, HttpMethod.POST)
         log.info("Send [task : {}] for perform", task.getTaskId());//TODO implement
         return true;
     }
